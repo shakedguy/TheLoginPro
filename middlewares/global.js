13dfb3b67f2b91a86b1ac18610af6614dbf5c887
shakedguy <shakedguy94@gmail.com>
2022-09-12 22:51:59 +0300

    update

--- middlewares/global.js
+++ middlewares/global.js
@@ -13,47 +13,47 @@ import cors from 'cors';
 const globalMiddlewares = express();
 globalMiddlewares.use(express.json());
 globalMiddlewares.use(cookieParser());
-// globalMiddlewares.use(csurf({ cookie: true }));
+globalMiddlewares.use(csurf({ cookie: true }));
 globalMiddlewares.use(bodyParser.json());
 globalMiddlewares.use(bodyParser.urlencoded({ extended: true }));
 globalMiddlewares.use(cors({ origin: '*' }));
-// globalMiddlewares.use(helmet.crossOriginResourcePolicy());
-// globalMiddlewares.use(helmet.dnsPrefetchControl());
-// globalMiddlewares.use(helmet.expectCt());
-// globalMiddlewares.use(helmet.frameguard());
-// globalMiddlewares.use(helmet.hidePoweredBy());
-// globalMiddlewares.use(helmet.hsts());
-// globalMiddlewares.use(helmet.ieNoOpen());
-// globalMiddlewares.use(helmet.noSniff());
-// globalMiddlewares.use(helmet.originAgentCluster());
-// globalMiddlewares.use(helmet.permittedCrossDomainPolicies());
-// globalMiddlewares.use(helmet.referrerPolicy());
-// globalMiddlewares.use(helmet.xssFilter());
+globalMiddlewares.use(helmet.crossOriginResourcePolicy());
+globalMiddlewares.use(helmet.dnsPrefetchControl());
+globalMiddlewares.use(helmet.expectCt());
+globalMiddlewares.use(helmet.frameguard());
+globalMiddlewares.use(helmet.hidePoweredBy());
+globalMiddlewares.use(helmet.hsts());
+globalMiddlewares.use(helmet.ieNoOpen());
+globalMiddlewares.use(helmet.noSniff());
+globalMiddlewares.use(helmet.originAgentCluster());
+globalMiddlewares.use(helmet.permittedCrossDomainPolicies());
+globalMiddlewares.use(helmet.referrerPolicy());
+globalMiddlewares.use(helmet.xssFilter());
 
-// globalMiddlewares.set('trust proxy', 1);
+globalMiddlewares.set('trust proxy', 1);
 
 globalMiddlewares.use(compression());
 
-// globalMiddlewares.use(
-// 	session({
-// 		genid: function (req) {
-// 			return uuidv4(); // use UUIDs for session IDs
-// 		},
-// 		store: new FirestoreStore({
-// 			dataset: Firestore,
-// 			kind: 'express-sessions',
-// 		}),
-// 		secret: process.env.SESSION_SECRET,
-// 		resave: false,
-// 		saveUninitialized: true,
-// 		cookie: { secure: true },
-// 	})
-// );
+globalMiddlewares.use(
+	session({
+		genid: function (req) {
+			return uuidv4(); // use UUIDs for session IDs
+		},
+		store: new FirestoreStore({
+			dataset: Firestore,
+			kind: 'express-sessions',
+		}),
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: true },
+	})
+);
 
-// globalMiddlewares.all('*', (req, res, next) => {
-// 	res.cookie('XSRF-TOKEN', req.csrfToken());
-// 	next();
-// });
+globalMiddlewares.all('*', (req, res, next) => {
+	res.cookie('XSRF-TOKEN', req.csrfToken());
+	next();
+});
 
 globalMiddlewares.use((req, res, next) => {
 	// Website you wish to allow to connect
